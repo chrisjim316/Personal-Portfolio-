7afad3c055121bdf3fc49ffc9b64aaf70206db24
Chris Jim <chrisjim316@gmail.com>
2017-08-06 00:23:24 +0800

    delete JS media queries

--- js/script.js
+++ js/script.js
@@ -10,25 +10,3 @@ $(document).ready(function(){
 
 $(document).on('click.bs.collapse.data-api touchstart.bs.collapse.data-api', '[data-toggle="collapse"]', function (e) { 
 });
-
-/* ipad screens media query in JavaScript */
-var mqls = [ // list of window.matchMedia() queries
-  window.matchMedia("(min-width: 768px)"),
-  window.matchMedia("(max-width: 800px)"),
-];
-
-if (mqls[0].matches && mqls[1].matches) {
-  $(".navbar").empty(); //empty navbar when bootstrap doesn't collapse it to prevent blocking the screen. 
-}
-
-/* iphone screens media query in JavaScript */
-var mqls2 = [ // list of window.matchMedia() queries
-  window.matchMedia("(min-width: 300px)"),
-  window.matchMedia("(max-width: 800px)"),
-];
-
-if (mqls2[0].matches && mqls2[1].matches) {
-  $(".time-wrapper").css("border-style", "none");
-}
-
-
