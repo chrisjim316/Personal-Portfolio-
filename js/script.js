ca9cd770010f5be3e1206f4988f464757ffb9190
Chris Jim <chrisjim316@gmail.com>
2018-08-06 01:35:58 +0100

    optimized memory storage and add four new buttons

--- js/script.js
+++ js/script.js
@@ -9,80 +9,98 @@ $(document).ready(function(){
   $("#showLess6").hide();
   $("#showLess7").hide();
   $("#showLess8").hide(); 
+  $("#showLess9").hide();
+  $("#showLess10").hide();
   
-  $(".scroll").click(function(event){
-    event.preventDefault();
-    $("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
-    $('.navbar-default a').removeClass('selected');
-    $(this).addClass('selected');
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+		$('.navbar-default a').removeClass('selected');
+		$(this).addClass('selected');
   });
   
   /* clicking 'show details' will turn the button into 'hide details' and vice versa 
-   * 8 projects in total so 16 buttons. 
+   * 10 projects in total so 20 buttons 
    */
-  $("#viewMore1").click( function() {
+  $("#viewMore1").on("click", function() {
     $("#viewMore1").hide();
     $("#showLess1").show();
   });
-  $("#showLess1").click( function () {
+  $("#showLess1").on("click", function() {
     $("#viewMore1").show();
     $("#showLess1").hide();
   });
-  $("#viewMore2").click( function() {
+  $("#viewMore2").on("click", function() {
     $("#viewMore2").hide();
     $("#showLess2").show();
   });
-  $("#showLess2").click( function () {
+  $("#showLess2").on("click", function() {
     $("#viewMore2").show();
     $("#showLess2").hide();
   });
-  $("#viewMore3").click( function() {
+  $("#viewMore3").on("click", function() {
     $("#viewMore3").hide();
     $("#showLess3").show();
   });
-  $("#showLess3").click( function () {
+  $("#showLess3").on("click", function() {
     $("#viewMore3").show();
     $("#showLess3").hide();
   });
-  $("#viewMore4").click( function() {
+  $("#viewMore4").on("click", function() {
     $("#viewMore4").hide();
     $("#showLess4").show();
   });
-  $("#showLess4").click( function () {
+  $("#showLess4").on("click", function() {
     $("#viewMore4").show();
     $("#showLess4").hide();
   });
-  $("#viewMore5").click( function() {
+  $("#viewMore5").on("click", function() {
     $("#viewMore5").hide();
     $("#showLess5").show();
   });
-  $("#showLess5").click( function () {
+  $("#showLess5").on("click", function() {
     $("#viewMore5").show();
     $("#showLess5").hide();
   });
-  $("#viewMore6").click( function() {
+  $("#viewMore6").on("click", function() {
     $("#viewMore6").hide();
     $("#showLess6").show();
   });
-  $("#showLess6").click( function () {
+  $("#showLess6").on("click", function() {
     $("#viewMore6").show();
     $("#showLess6").hide();
   });
-  $("#viewMore7").click( function() {
+  $("#viewMore7").on("click", function() {
     $("#viewMore7").hide();
     $("#showLess7").show();
   });
-  $("#showLess7").click( function () {
+  $("#showLess7").on("click", function() {
     $("#viewMore7").show();
     $("#showLess7").hide();
   });
-  $("#viewMore8").click( function() {
+  $("#viewMore8").on("click", function() {
     $("#viewMore8").hide();
     $("#showLess8").show();
   });
-  $("#showLess8").click( function () {  
+  $("#showLess8").on("click", function() { 
     $("#viewMore8").show();
     $("#showLess8").hide();
+  });
+  $("#viewMore9").on("click", function() {
+    $("#viewMore9").hide();
+    $("#showLess9").show();
+  });
+  $("#showLess9").on("click", function() { 
+    $("#viewMore9").show();
+    $("#showLess9").hide();
+  });
+  $("#viewMore10").on("click", function() {
+    $("#viewMore10").hide();
+    $("#showLess10").show();
+  });
+  $("#showLess10").on("click", function() { 
+    $("#viewMore10").show();
+    $("#showLess10").hide();
   });
 });
 
